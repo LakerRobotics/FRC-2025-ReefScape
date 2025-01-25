2943ba151120ac6742cc3bd49eae158f75eec524
Richard G. Topolewski <haileyhutsell3@gmail.com>
2025-01-25 11:37:21 -0500

    moving to Techhounds Code
    Removed our 2024 code for substystem and

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,6 +12,221 @@ import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
 //import frc.lib.PIDGains;
 
+import java.util.List;
+
+import com.pathplanner.lib.config.ModuleConfig;
+import com.pathplanner.lib.config.RobotConfig;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+
+
+public class Constants {
+    public static final double LOOP_TIME = 0.020; // 20ms
+
+    public static final class Drivetrain {
+
+        public static final int LEFT_PRIMARY_MOTOR_ID = 3;
+        public static final int LEFT_SECONDARY_MOTOR_ID = 4;
+        public static final int RIGHT_PRIMARY_MOTOR_ID = 1;
+        public static final int RIGHT_SECONDARY_MOTOR_ID = 2;
+
+        public static final boolean LEFT_DRIVE_MOTORS_INVERTED = false;
+        public static final boolean RIGHT_DRIVE_MOTORS_INVERTED = true;
+
+        /** Distance between left and right wheels. */
+        public static final double TRACK_WIDTH_METERS = Units.inchesToMeters(22);
+
+        public static final double GEARING = 8.45;
+        public static final double WHEEL_RADIUS_METERS = Units.inchesToMeters(3) / 0.9788265228;
+        public static final double WHEEL_CIRCUMFERENCE = 2 * Math.PI * WHEEL_RADIUS_METERS;
+        public static final double ENCODER_ROTATIONS_TO_METERS = WHEEL_CIRCUMFERENCE / GEARING;
+
+        public static final int CURRENT_LIMIT = 60;
+        public static final DCMotor GEARBOX_REPR = DCMotor.getNEO(2);
+        public static final double MOI = 2;
+        public static final double MASS_KG = 32;
+        public static final double WHEEL_COF = 1.0;
+
+        public static final double VELOCITY_kP = RobotBase.isReal() ? 1.5 : 1.4078;
+        public static final double VELOCITY_kI = 0.0;
+        public static final double VELOCITY_kD = 0.0;
+        public static final double kS = RobotBase.isReal() ? 0.29971 : 0.057644;
+        public static final double kV_LINEAR = RobotBase.isReal() ? 2.4 : 2.159;
+        public static final double kA_LINEAR = RobotBase.isReal() ? 0.56555 : 0.24174;
+        public static final double kV_ANGULAR = RobotBase.isReal() ? 1.2427 : 2.1403;
+        public static final double kA_ANGULAR = RobotBase.isReal() ? 0.0859 : 0.42056;
+
+        public static final double POSITION_kP = 1.5;
+        public static final double POSITION_kI = 0.0;
+        public static final double POSITION_kD = 0.0;
+        public static final double ROTATION_kP = 2.0;
+        public static final double ROTATION_kI = 0.0;
+        public static final double ROTATION_kD = 0.0;
+
+        public static final double RAMSETE_B = 3;
+        public static final double RAMSETE_ZETA = 0.7;
+
+        public static final double MAX_DRIVING_VELOCITY_METERS_PER_SECOND = 5;
+        public static final double MAX_DRIVING_ACCELERATION_METERS_PER_SECOND_SQUARED = 8;
+        public static final double MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND = 20;
+        public static final double MAX_ANGULAR_ACCELERATION_RADIANS_PER_SECOND_SQUARED = 40;
+
+        public static final TrapezoidProfile.Constraints MOVEMENT_CONSTRAINTS = new TrapezoidProfile.Constraints(
+                MAX_DRIVING_VELOCITY_METERS_PER_SECOND,
+                MAX_DRIVING_ACCELERATION_METERS_PER_SECOND_SQUARED);
+        public static final TrapezoidProfile.Constraints ROTATION_CONSTRAINTS = new TrapezoidProfile.Constraints(
+                MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND,
+                MAX_ANGULAR_ACCELERATION_RADIANS_PER_SECOND_SQUARED);
+
+        public static final DifferentialDriveKinematics KINEMATICS = new DifferentialDriveKinematics(
+                TRACK_WIDTH_METERS);
+
+        public static final RobotConfig ROBOT_CONFIG = new RobotConfig(
+                MASS_KG, MOI,
+                new ModuleConfig(
+                        WHEEL_RADIUS_METERS,
+                        MAX_DRIVING_VELOCITY_METERS_PER_SECOND,
+                        WHEEL_COF, GEARBOX_REPR, CURRENT_LIMIT, 2),
+                TRACK_WIDTH_METERS);
+    }
+
+    public static final class Elevator {
+        public static enum ElevatorPosition {
+            BOTTOM(0.0698),
+            INTAKE_PREP(0.55),
+            INTAKE(0.355),
+            ALGAE_L2(0.884),
+            ALGAE_L3(1.234),
+
+            L1(0.323),
+            L2(0.31),
+            L3(0.70),
+            L4(1.27),
+            TOP(1.57);
+
+            public final double value;
+
+            private ElevatorPosition(double value) {
+                this.value = value;
+            }
+        }
+
+        public static final double MOTION_LIMIT = 0.3;
+
+        public static final double SCORING_MOVEMENT = -0.25;
+
+        public static final int MOTOR_ID = 5;
+        public static final boolean MOTOR_INVERTED = false;
+
+        public static final DCMotor MOTOR_GEARBOX_REPR = DCMotor.getNEO(1);
+        public static final double GEARING = 5.0;
+        public static final double MASS_KG = Units.lbsToKilograms(20);
+        public static final double DRUM_RADIUS_METERS = Units.inchesToMeters(1.32) / 2.0; // TODO
+        public static final double DRUM_CIRCUMFERENCE = 2.0 * Math.PI * DRUM_RADIUS_METERS;
+        public static final double ENCODER_ROTATIONS_TO_METERS = DRUM_CIRCUMFERENCE / GEARING;
+
+        public static final double MIN_HEIGHT_METERS = 0.005; // TODO
+        public static final double MAX_HEIGHT_METERS = 1.57; // TODO
+
+        public static final int CURRENT_LIMIT = 60;
+
+        public static final double kP = 50; // TODO
+        public static final double kI = 0; // TODO
+        public static final double kD = 5; // TODO
+        public static final double kS = 0.095388; // TODO
+        public static final double kG = 0.54402; // TODO
+        public static final double kV = 7.43; // TODO
+        public static final double kA = 1.0; // TODO
+        public static final double TOLERANCE = 0.02;
+
+        public static final double MAX_VELOCITY_METERS_PER_SECOND = 1.3; // TODO
+        public static final double MAX_ACCELERATION_METERS_PER_SECOND_SQUARED = 3; // TODO
+        public static final TrapezoidProfile.Constraints MOVEMENT_CONSTRAINTS = new TrapezoidProfile.Constraints(
+                MAX_VELOCITY_METERS_PER_SECOND, MAX_ACCELERATION_METERS_PER_SECOND_SQUARED);
+    }
+
+    public static final class Arm {
+        public static enum ArmPosition {
+            BOTTOM(-Math.PI / 2.0 + Units.degreesToRadians(5)),
+            HORIZONTAL(0),
+            L1(0),
+            L2(Units.degreesToRadians(55)), // reef angle
+            L3(Units.degreesToRadians(55)),
+            L4(1.033),
+            TOP(Math.PI / 2.0);
+
+            public final double value;
+
+            private ArmPosition(double value) {
+                this.value = value;
+            }
+        }
+
+        public static final double MOTION_LIMIT = -0.7;
+        public static final double SCORING_MOVEMENT = -0.8;
+
+        public static final int MOTOR_ID = 12;
+        public static final boolean MOTOR_INVERTED = true;
+
+        public static final DCMotor MOTOR_GEARBOX_REPR = DCMotor.getNEO(1);
+        public static final double GEARING = 40.0; // TODO
+        public static final double MASS_KG = Units.lbsToKilograms(10); // TODO
+        public static final double COM_DISTANCE_METERS = Units.inchesToMeters(6); // TODO
+        public static final double MOI = SingleJointedArmSim.estimateMOI(COM_DISTANCE_METERS, MASS_KG);
+        public static final double ENCODER_ROTATIONS_TO_METERS = 2 * Math.PI / GEARING;
+
+        public static final double MIN_ANGLE_RADIANS = -Math.PI / 2.0;
+        public static final double MAX_ANGLE_RADIANS = Math.PI / 2.0;
+
+        public static final int CURRENT_LIMIT = 50;
+
+        public static final double kP = 10; // TODO
+        public static final double kI = 0; // TODO
+        public static final double kD = 0; // TODO
+        public static final double kS = 0.017964; // TODO
+        public static final double kG = 0.321192; // TODO
+        public static final double kV = 0.876084;// TODO
+        public static final double kA = 0.206676;// TODO
+        public static final double TOLERANCE = 0.02;
+
+        public static final double MAX_VELOCITY_METERS_PER_SECOND = 8; // TODO
+        public static final double MAX_ACCELERATION_METERS_PER_SECOND_SQUARED = 4; // TODO
+        public static final TrapezoidProfile.Constraints MOVEMENT_CONSTRAINTS = new TrapezoidProfile.Constraints(
+                MAX_VELOCITY_METERS_PER_SECOND, MAX_ACCELERATION_METERS_PER_SECOND_SQUARED);
+    }
+
+    public static final class Intake {
+        public static final int MOTOR_ID = 13;
+        public static final boolean MOTOR_INVERTED = true;
+        public static final int CURRENT_LIMIT = 60;
+    }
+
+    public static final class Climber {
+        public static final int MOTOR_ID = 6;
+        public static final boolean MOTOR_INVERTED = false;
+        public static final int CURRENT_LIMIT = 60;
+
+        public static final double MIN_POSITION_METERS = 0.0;
+        public static final double MAX_POSITION_METERS = 1.0; // TODO
+
+        public static final double GEARING = 64.0;
+        public static final double MASS_KG = Units.lbsToKilograms(80); // robot weight
+        public static final double SPOOL_RADIUS_METERS = Units.inchesToMeters(0.5);
+        public static final double SPOOL_CIRCUMFERENCE = 2.0 * Math.PI * SPOOL_RADIUS_METERS;
+        public static final double ENCODER_ROTATIONS_TO_METERS = SPOOL_CIRCUMFERENCE * GEARING;
+
+    }
+
+    public static final class Controls {
+        public static final double JOYSTICK_INPUT_RATE_LIMIT = 15.0;
+        public static final double JOYSTICK_INPUT_DEADBAND = 0.1;
+        public static final double JOYSTICK_CURVE_EXP = 2;
+        public static final double JOYSTICK_ROT_CURVE_EXP = 3;
+        public static final double JOYSTICK_ROT_LIMIT = 0.8;
+    }
+}
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -24,7 +239,8 @@ public final class Constants {
   public static final class OIConstants {
     public static final int kDriverControllerPort = 0;
     public static final double kDriveDeadband = 0.05;
-    public static final double kTriggerButtonThreshold = 0.5;
+    public static final double kTriggerButtonThreshold = 0.5
+//import frc.robot.Constants.NeoMotorConstants;;
   }
 
   public static final class Arm {
