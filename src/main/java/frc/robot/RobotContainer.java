3eaf2d2be4f6744a0483d9777a383f142082b40e
Seth5053 <123704028+Seth5053@users.noreply.github.com>
2025-02-26 14:17:17 -0500

    added new trigger
    added new trigger code for arm & elevator it seems (coach Rich for changes Hailey made?)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -37,7 +37,8 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand; 
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -256,7 +257,16 @@ private MechanismLigament2d fromRobot = root
     new Trigger( 
             () -> 
                  Math.abs(rightJoystick.getRightY()) > 0.03 )
-                 .onTrue(new RunCommand(() -> intake.setRollerVoltage(rightJoystick.getRightY()),intake));      
+                 .onTrue(new RunCommand(() -> intake.setRollerVoltage(rightJoystick.getRightY()),intake));
+
+  
+    new Trigger(
+      () -> 
+                 Math.abs(rightJoystick.getRightX()) > 0.03 )
+                 .onTrue(new RunCommand(() -> arm.setVoltage(rightJoystick.getRightX()),arm)); 
+
+    new JoystickButton(rightJoystick, PS4Controller.Button.kSquare.value)
+    .onTrue(new InstantCommand(() -> elevator.setTargetPosition(Constants.Elevator.autoPrepareCoralScoreCommand.L2)));
 
     /* 
     // set up arm preset positions
